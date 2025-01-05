e89d84696c85311d034ca7f54980de3463dfcfc1
prathamm2506 <fs20if005@gmail.com>
2025-01-05 18:08:21 +0530

    adding data

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import './Project.css'
+import img1 from '../../assets/texttoimg.png'
+import img2 from '../../assets/dobot.png'
+import img3 from '../../assets/lung.png'
+import img4 from '../../assets/clustering.png'
+import img5 from '../../assets/netmonitor.png'
+import img6 from '../../assets/droneairgear.png'
+
+
 
 const Project = () => {
   return (
@@ -8,41 +16,41 @@ const Project = () => {
 
         <div className='flex flex-row mt-12 justify-between respoproj'>
             <div className='projcard'>
-                <img src="https://media.beehiiv.com/cdn-cgi/image/format=auto,width=800,height=421,fit=scale-down,onerror=redirect/uploads/asset/file/72421ef0-c5eb-4829-b312-9615d61945a8/freelance-designer-101-Design-Process-Figma-Organization.png" alt="IMG" />
-                <a href="#"><h3>Freelance Designer 101: Design Process & Figma File Organization</h3></a>
+                <img src={img1} alt="IMG" />
+                <a href="https://github.com/prathamm2506/text-to-image"><h3>Text to Image</h3></a>
                 <p>November 2024</p>
             </div>
 
             <div className='projcard'>
-                <img src="https://media.beehiiv.com/cdn-cgi/image/format=auto,width=800,height=421,fit=scale-down,onerror=redirect/uploads/asset/file/72421ef0-c5eb-4829-b312-9615d61945a8/freelance-designer-101-Design-Process-Figma-Organization.png" alt="IMG" />
-                <a href="#"><h3>Freelance Designer 101: Design Process & Figma File Organization</h3></a>
-                <p>November 2024</p>
+                <img src={img2} alt="IMG" />
+                <a href="https://github.com/prathamm2506/dobot-magician"><h3>Robotic Arm with AI Vision</h3></a>
+                <p>February 2023</p>
             </div>
 
             <div className='projcard'>
-                <img src="https://media.beehiiv.com/cdn-cgi/image/format=auto,width=800,height=421,fit=scale-down,onerror=redirect/uploads/asset/file/72421ef0-c5eb-4829-b312-9615d61945a8/freelance-designer-101-Design-Process-Figma-Organization.png" alt="IMG" />
-                <a href="#"><h3>Freelance Designer 101: Design Process & Figma File Organization</h3></a>
+                <img src={img3} alt="IMG" />
+                <a href="https://github.com/prathamm2506/lung_cancer_detection"><h3>CNN Based Lung Cancer Detection</h3></a>
                 <p>November 2024</p>
             </div>
         </div>
 
         <div className='flex mt-12 justify-between respoproj'>
             <div className='projcard'>
-                <img src="https://media.beehiiv.com/cdn-cgi/image/format=auto,width=800,height=421,fit=scale-down,onerror=redirect/uploads/asset/file/72421ef0-c5eb-4829-b312-9615d61945a8/freelance-designer-101-Design-Process-Figma-Organization.png" alt="IMG" />
-                <a href="#"><h3>Freelance Designer 101: Design Process & Figma File Organization</h3></a>
-                <p>November 2024</p>
+                <img src={img4} alt="IMG" />
+                <a href="https://github.com/prathamm2506/Clustering-For-Delivery-System"><h3>Clustering System for Dilivery Platform</h3></a>
+                <p>October 2024</p>
             </div>
 
             <div className='projcard'>
-                <img src="https://media.beehiiv.com/cdn-cgi/image/format=auto,width=800,height=421,fit=scale-down,onerror=redirect/uploads/asset/file/72421ef0-c5eb-4829-b312-9615d61945a8/freelance-designer-101-Design-Process-Figma-Organization.png" alt="IMG" />
-                <a href="#"><h3>Freelance Designer 101: Design Process & Figma File Organization</h3></a>
-                <p>November 2024</p>
+                <img src={img5} alt="IMG" />
+                <a href="https://github.com/prathamm2506/Network_Monitor"><h3>Network Monitoring and Visualization</h3></a>
+                <p>October 2024</p>
             </div>
 
             <div className='projcard'>
-                <img src="https://media.beehiiv.com/cdn-cgi/image/format=auto,width=800,height=421,fit=scale-down,onerror=redirect/uploads/asset/file/72421ef0-c5eb-4829-b312-9615d61945a8/freelance-designer-101-Design-Process-Figma-Organization.png" alt="IMG" />
-                <a href="#"><h3>Freelance Designer 101: Design Process & Figma File Organization</h3></a>
-                <p>November 2024</p>
+                <img src={img6} alt="IMG" />
+                <a href="https://droneairgear.com/"><h3>One Stop Solution for Drones</h3></a>
+                <p>June 2023</p>
             </div>
         </div>
     </div>
